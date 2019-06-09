3cafaf9d7a286f45a52e2dfeed5790ac4e71dc87
vuongngocanh189 <bttngocanh@gmail.com>
2019-06-09 21:24:30 +0300

    fix styles

--- includes/ak-github-repos-scripts.php
+++ includes/ak-github-repos-scripts.php
@@ -1,6 +1,6 @@
 <?php
 function ak_add_scripts() {
-	wp_enqueue_style('ak-main-style', plugins_url() . '/ak-github-repo-list/css/ak-style.css', array(), '1.1');
+	wp_enqueue_style('ak-main-style', plugins_url() . '/ak-github-repo/css/ak-style.css', array(), '1.3');
 }
 add_action('wp_enqueue_scripts', 'ak_add_scripts');
 
